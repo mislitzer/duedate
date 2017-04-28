03e36ac47097a016de522cbf99b3154636a851be
mislitzer <katjamair@katjas-mbp.home>
2017-04-28 21:54:11 +0200

    Merged Branches and added Language Support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,14 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import {HttpModule, Http} from '@angular/http';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: Http) {
+    return new TranslateHttpLoader(http);
+}
+
 @NgModule({
   declarations: [
     MyApp,
@@ -21,6 +29,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [Http]
+            }
+        }),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
