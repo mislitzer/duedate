d32929df9e8798453c8b1b28bfdce267c252cbb2
mislitzer1991 <Martin@Martins-iMac.local>
2017-06-05 00:25:03 +0200

    Config

--- src/environments/configuration.ts
+++ src/environments/configuration.ts
@@ -5,7 +5,7 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class Configuration {
     private _user:any;
-    private _serviceBase:string = "http://localhost:9000";
+    private _serviceBase:string = "http://duedate.web.fh-kufstein.ac.at";
     private _module:any;
     private _labels:any;
 
